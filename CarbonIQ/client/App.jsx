d141ea0a819b10b2495f58ef8283f3fa6b263699
Mike Muteithia <mikemuteithia@gmail.com>
2025-10-30 05:05:25 +0300

    update App, AIecoCoach, Activities, and Goals pages with new UI and logic improvements

--- CarbonIQ/client/App.jsx
+++ CarbonIQ/client/App.jsx
@@ -5,6 +5,9 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import CarbonIQDashboard from "./pages/Dashboard";
 import MyAssetsPage from "./pages/MyAssets";
+import Activities from "./pages/Activities"; // Import the Activities page
+import AIecoCoach from "./pages/AIecoCoach"; // Import AI EcoCoach page
+import Goals from "./pages/Goals"; // Import Goals page
 import Sidebar from "./components/Sidebar";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -47,10 +50,54 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        {/* Add Activities route */}
+        <Route 
+          path="/activities"
+          element={
+            <ProtectedRoute>
+              <div className="flex min-h-screen bg-gray-50">
+                <Sidebar />
+                <main className="flex-1 ml-64 pl-4 pr-8 py-4">
+                  <Activities />
+                </main>
+              </div>
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Add AI EcoCoach route (if the page exists) */}
+        <Route 
+          path="/ecocoach"
+          element={
+            <ProtectedRoute>
+              <div className="flex min-h-screen bg-gray-50">
+                <Sidebar />
+                <main className="flex-1 ml-64 pl-4 pr-8 py-4">
+                  <AIecoCoach />
+                </main>
+              </div>
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Add Goals route (if the page exists) */}
+        <Route 
+          path="/goals"
+          element={
+            <ProtectedRoute>
+              <div className="flex min-h-screen bg-gray-50">
+                <Sidebar />
+                <main className="flex-1 ml-64 pl-4 pr-8 py-4">
+                  <Goals />
+                </main>
+              </div>
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
